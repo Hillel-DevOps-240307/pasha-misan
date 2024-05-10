c2e9563b39df2e9ff2f76aa3ed803558073115dd
PashaMisan <redmi2.misan@gmail.com>
2024-05-10 20:20:00 +0300

    Refactoring

--- terraform/dev/variables.tf
+++ terraform/dev/variables.tf
@@ -2,13 +2,3 @@ variable "env" {
   description = "Env"
   default     = "dev"
 }
-
-variable "app-subnet-id" {
-  description = "APP subnet id"
-  default     = "subnet-0d75f6a5e122b2ed3"
-}
-
-variable "db-subnet-id" {
-  description = "DB subnet id"
-  default     = "subnet-0eb7d7cf58c3e85d8"
-}
--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -2,11 +2,11 @@ module "prod-vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name                          = "Prod-vpc"
-  cidr                          = "192.168.0.0/24"
+  cidr                          = var.cidr
   manage_default_security_group = false
   map_public_ip_on_launch       = true
 
-  azs             = ["eu-central-1a"]
+  azs             = var.azs
   private_subnets = ["192.168.0.0/26"]
   public_subnets  = ["192.168.0.64/26", "192.168.0.128/26", "192.168.0.192/26"]
 
@@ -20,11 +20,11 @@ module "dev-vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name                          = "Dev-vpc"
-  cidr                          = "192.168.0.0/24"
+  cidr                          = var.cidr
   manage_default_security_group = false
   map_public_ip_on_launch       = true
 
-  azs             = ["eu-central-1a"]
+  azs             = var.azs
   private_subnets = ["192.168.0.0/26"]
   public_subnets  = ["192.168.0.64/26"]
 
